2027ec9a25b72e6d7820b534f91d395fddd253c4
MinoUni <maks.reva.ua@gmail.com>
2022-04-24 16:27:35 +0300

    Create a constant for playersNumb

--- src/main/java/com/eleks/academy/whoami/App.java
+++ src/main/java/com/eleks/academy/whoami/App.java
@@ -8,12 +8,14 @@ public class App {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		
+		final int playersNumb = 3;
+		
 		ServerImpl server = new ServerImpl(555);
 
 		Game game = server.startGame();
 
 		try {
-			for(int i = 0; i < 3; i++) {
+			for(int i = 0; i < playersNumb; i++) {
 				var socket = server.waitForPlayers(game);
 				System.out.println("Player connected, starting a identify process.");
 				Thread thr = new Thread(new Runnable() {
@@ -26,7 +28,7 @@ public class App {
 				System.out.println(thr.getName());
 			}
 //			Think about some improvement
-			while (game.countPlayers() != 3) {	//<--- weak point
+			while (game.countPlayers() != playersNumb) {	//<--- weak point
 				Thread.sleep(1000);
 			}
 			game.init();
