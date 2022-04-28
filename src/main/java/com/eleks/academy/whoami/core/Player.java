68359ad935b8fd39e6829d9881b6381c03ba9013
Dmitry <dmitrymishnev@gmail.com>
2022-04-28 10:59:44 +0300

    changed comeback type in methods at Player to Future<String> and realisation at ClientPlayer and RandomPlayer

--- src/main/java/com/eleks/academy/whoami/core/Player.java
+++ src/main/java/com/eleks/academy/whoami/core/Player.java
@@ -9,18 +9,18 @@ public interface Player {
 	Future<String> suggestCharacter();
 
 	// TODO: return Future<String>
-	String getQuestion();
-	
-	// TODO: return Future<String>
-	String answerQuestion(String question, String character);
-	
-	// TODO: return Future<String>
-	String getGuess();
-	
-	// TODO: return Future<String>
-	boolean isReadyForGuess();
+	Future<String> getQuestion();
 
 	// TODO: return Future<String>
-	String answerGuess(String guess, String character);
+	Future<String> answerQuestion(String question, String character);
+
+	// TODO: return Future<String>
+	Future<String> getGuess();
+
+	// TODO: return Future<String>
+	Future<Boolean> isReadyForGuess();
+
+	// TODO: return Future<String>
+	Future<String> answerGuess(String guess, String character);
 	
 }
--- src/main/java/com/eleks/academy/whoami/networking/client/ClientPlayer.java
+++ src/main/java/com/eleks/academy/whoami/networking/client/ClientPlayer.java
@@ -1,139 +1,150 @@
 package com.eleks.academy.whoami.networking.client;
 
+import com.eleks.academy.whoami.core.Player;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-
-import com.eleks.academy.whoami.core.Player;
+import java.util.concurrent.*;
 
 public class ClientPlayer implements Player, AutoCloseable {
+    private String name = "";
+    private BufferedReader reader;
+    private PrintStream writer;
+    private String suggestedCharacter = "";
 
-	private BufferedReader reader;
-	private PrintStream writer;
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
 
-	private final ExecutorService executor = Executors.newSingleThreadExecutor();
+    public ClientPlayer(Socket socket) throws IOException {
+        this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        this.writer = new PrintStream(socket.getOutputStream());
+    }
 
-	public ClientPlayer(Socket socket) throws IOException {
-		this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		this.writer = new PrintStream(socket.getOutputStream());
-	}
+    @Override
+    public Future<String> getName() {
+        return executor.submit(this::askName);
+    }
 
-	@Override
-	public Future<String> getName() {
-		// TODO: save name for future
-		return executor.submit(this::askName);
-	}
+    private String askName() {
+        if (name.isBlank()) {
+            try {
+                writer.println("Please, name yourself.");
+                name = reader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return name;
+    }
 
-	private String askName() {
-		String name = "";
+    @Override
+    public Future<String> getQuestion() {
+        Callable<String> askQuestion = () -> {
+            String question = "";
 
-		try {
-			writer.println("Please, name yourself.");
-			name = reader.readLine();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return name;
-	}
+            try {
+                writer.println("Ask your question: ");
+                question = reader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return question;
+        };
+        return executor.submit(askQuestion);
+    }
 
-	@Override
-	public String getQuestion() {
-		String question = "";
+    @Override
+    public Future<String> answerQuestion(String question, String character) {
+        Callable<String> answerQuestion = () -> {
+            String answer = "";
+            try {
+                writer.println("Answer second player question: " + question + "Character is:" + character);
+                answer = reader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return answer;
+        };
+        return executor.submit(answerQuestion);
+    }
 
-		try {
-			writer.println("Ask your questinon: ");
-			question = reader.readLine();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return question;
-	}
+    @Override
+    public Future<String> getGuess() {
+        Callable<String> guess = () -> {
+            String answer = "";
 
-	@Override
-	public String answerQuestion(String question, String character) {
-		String answer = "";
-		
-		try {
-			writer.println("Answer second player question: " + question + "Character is:"+ character);
-			answer = reader.readLine();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		return answer;
-	}
+            try {
+                writer.println("Write your guess: ");
+                answer = reader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return answer;
+        };
+        return executor.submit(guess);
+    }
 
-	@Override
-	public String getGuess() {
-		String answer = "";
-		
-	
-		try {
-			writer.println("Write your guess: ");
-			answer = reader.readLine();
-		} catch (IOException e) {
+    @Override
+    public Future<Boolean> isReadyForGuess() {
+        Callable<Boolean> isReadyForGuess = () -> {
+            String answer = "";
 
-			e.printStackTrace();
-		}
-		return answer;
-	}
+            try {
+                writer.println("Are you ready to guess? ");
+                answer = reader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-	@Override
-	public boolean isReadyForGuess() {
-		String answer = "";
-		
-		try {
-			writer.println("Are you ready to guess? ");
-			answer = reader.readLine();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		return answer.equals("Yes") ? true : false;
-	}
+            return answer.equals("Yes");
+        };
+        return executor.submit(isReadyForGuess);
+    }
 
-	@Override
-	public String answerGuess(String guess, String character) {
-		String answer = "";
-		
-		try {
-			writer.println("Write your answer: ");
-			answer = reader.readLine();
-		} catch (IOException e) {
+    @Override
+    public Future<String> answerGuess(String guess, String character) {
+        Callable<String> answerGuess = () -> {
+            String answer = "";
 
-			e.printStackTrace();
-		}
-		return answer;
-	}
+            try {
+                writer.println("Write your answer: ");
+                answer = reader.readLine();
+            } catch (IOException e) {
 
-	@Override
-	public Future<String> suggestCharacter() {
-		return executor.submit(this::doSuggestCharacter);
-	}
+                e.printStackTrace();
+            }
+            return answer;
+        };
+        return executor.submit(answerGuess);
+    }
 
-	private String doSuggestCharacter() {
-		try {
-			return reader.readLine();
-		} catch (IOException e) {
-			e.printStackTrace();
-			return "";
-		}
-	}
+    @Override
+    public Future<String> suggestCharacter() {
+        return executor.submit(this::doSuggestCharacter);
+    }
 
-	@Override
-	public void close() {
-		executor.shutdown();
-		try {
-			executor.awaitTermination(5, TimeUnit.SECONDS);
-		} catch (InterruptedException e) {
-			Thread.currentThread().interrupt();
-		}
-	}
+    private String doSuggestCharacter() {
+        if (suggestedCharacter.isBlank()) {
+            try {
+                return reader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+
+            }
+        }
+        return suggestedCharacter;
+    }
+
+    @Override
+    public void close() {
+        executor.shutdown();
+        try {
+            executor.awaitTermination(5, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
 
 }
