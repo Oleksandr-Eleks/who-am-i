695eb7203bd19d33c499a8be1171ec624c87b1fe
Dmitry <dmitrymishnev@gmail.com>
2022-04-28 17:45:52 +0300

    comeback to version of Max Kit

--- src/main/java/com/eleks/academy/whoami/networking/server/ServerImpl.java
+++ src/main/java/com/eleks/academy/whoami/networking/server/ServerImpl.java
@@ -11,73 +11,56 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 public class ServerImpl implements Server {
 
-    private List<String> characters = List.of("Batman", "Superman");
-    private List<String> questions = List.of("Am i a human?", "Am i a character from a movie?");
-    private List<String> guesses = List.of("Batman", "Superman");
-    private static final int DURATION = 2;
-    private static final TimeUnit UNIT = TimeUnit.SECONDS;
+	private final ServerSocket serverSocket;
+	private final List<Socket> openSockets = new ArrayList<>();
+	private List<String> characters = List.of("Batman", "Superman");
+	private List<String> questions = List.of("Am i a human?", "Am i a character from a movie?");
+	private List<String> guessess = List.of("Batman", "Superman");
+	private RandomGame game = new RandomGame(characters);
 
-    private RandomGame game = new RandomGame(characters);
+	public ServerImpl(int port) throws IOException {
+		this.serverSocket = new ServerSocket(port);
+	}
 
-    private final ServerSocket serverSocket;
-    private final List<Socket> openSockets = new ArrayList<>();
+	@Override
+	public Game startGame() throws IOException {
+		game.addPlayer(new RandomPlayer("Bot", characters, questions, guessess));
+		System.out.println("Server starts");
+		System.out.println("Waiting for a client connect....");
+		return game;
+	}
 
-    public ServerImpl(int port) throws IOException {
-        this.serverSocket = new ServerSocket(port);
-    }
+	@Override
+	public Socket waitForPlayer(Game game) throws IOException {
+		Socket player = serverSocket.accept();
+		openSockets.add(player);
+		return player;
+	}
 
-    @Override
-    public Game startGame() throws IOException {
-        game.addPlayer(new RandomPlayer("Bot", characters, questions, guesses));
-        System.out.println("Server starts");
-        System.out.println("Waiting for a client connect....");
-        return game;
-    }
+	@Override
+	public void addPlayer(Player player) {
+		game.addPlayer(player);
+		System.out.println("Player: " + player.getName() + " Connected to the game!");
 
-    @Override
-    public Socket waitForPlayer(Game game) throws IOException {
-        Socket player = serverSocket.accept();
-        openSockets.add(player);
-        return player;
-    }
+	}
 
-    @Override
-    public void addPlayer(Player player) {
-        String name = "";
-        try {
-            name = player.getName().get(DURATION, UNIT);
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-            Thread.currentThread().interrupt();
-        } catch (TimeoutException e) {
-            System.err.printf("Player did not answer within %d %s%n", DURATION, UNIT);
-        }
-        if (!name.isBlank()) {
-            game.addPlayer(player);
-            System.out.println("Player: " + name + " Connected to the game!");
-        }
-    }
+	@Override
+	public void stopServer(Socket clientSocket, BufferedReader reader) throws IOException {
+		clientSocket.close();
+		reader.close();
+	}
 
-    @Override
-    public void stopServer(Socket clientSocket, BufferedReader reader) throws IOException {
-        clientSocket.close();
-        reader.close();
-    }
-
-    public void stop() {
-        for (Socket s : openSockets) {
-            try {
-                s.close();
-            } catch (IOException e) {
-                System.err.printf("Could not close a socket (%s)%n", e.getMessage());
-            }
-        }
-    }
+	public void stop() {
+		for (Socket s : openSockets) {
+			try {
+				s.close();
+			} catch (IOException e) {
+				System.err.println(String.format("Could not close a socket (%s)", e.getMessage()));
+			}
+		}
+	}
 
 }
