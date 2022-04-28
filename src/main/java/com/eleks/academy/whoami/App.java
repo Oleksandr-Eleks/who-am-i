2e19d83a346feac90efa8063ffdd00df5a6bef55
MinoUni <maks.reva.ua@gmail.com>
2022-04-28 17:14:39 +0300

    refactor App, delete AppTest

--- src/main/java/com/eleks/academy/whoami/App.java
+++ src/main/java/com/eleks/academy/whoami/App.java
@@ -8,32 +8,13 @@ public class App {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		
-		final int playersNumb = 3;
-		
-		ServerImpl server = new ServerImpl(555);
-
-		Game game = server.startGame();
+		ServerImpl server = new ServerImpl(1919);
 
 		try {
-			for(int i = 0; i < playersNumb; i++) {
-				var socket = server.waitForPlayers(game);
-				System.out.println("Player connected, starting a identify process.");
-				Thread thr = new Thread(new Runnable() {
-					@Override
-					public void run() {
-						server.identify(socket);
-					}
-				});
-				thr.start();
-				System.out.println(thr.getName());
-			}
-//			Think about some improvement
-			while (game.countPlayers() != playersNumb) {	//<--- weak point
-				Thread.sleep(1000);
-			}
+			Game game = server.startGame();
 			game.init();
 		} finally {
-			System.out.println("Close sockets");
+			server.stopServer();
 			server.stop();
 		}
 	}
