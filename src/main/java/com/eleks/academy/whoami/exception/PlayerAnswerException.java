b9cefa90d402e0e362ac77eab91d8455925b7fd7
Dmitry <dmitrymishnev@gmail.com>
2022-05-01 17:48:02 +0300

    refactor custom Exception

--- src/main/java/com/eleks/academy/whoami/exception/PlayerAnswerException.java
+++ src/main/java/com/eleks/academy/whoami/exception/PlayerAnswerException.java
@@ -3,5 +3,6 @@ package com.eleks.academy.whoami.exception;
 public class PlayerAnswerException extends RuntimeException {
     public PlayerAnswerException(String message, Throwable cause) {
         super(message, cause);
+        cause.printStackTrace();
     }
 }
