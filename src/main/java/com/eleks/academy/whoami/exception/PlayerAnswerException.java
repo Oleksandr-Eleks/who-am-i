280e43e163a6ed198f0d57e6007f14ba15663d6b
Dmitry <dmitrymishnev@gmail.com>
2022-05-01 11:13:25 +0300

    added custom exception

---
+++ src/main/java/com/eleks/academy/whoami/exception/PlayerAnswerException.java
@@ -0,0 +1,7 @@
+package com.eleks.academy.whoami.exception;
+
+public class PlayerAnswerException extends RuntimeException {
+    public PlayerAnswerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
