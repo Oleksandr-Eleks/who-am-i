8325bb326c19e421ee83f4ea48d01f8971bd1a8c
Serhii Dutchyn <serhiydutchyn@gmail.com>
2022-05-03 17:33:14 +0300

    Future for player's method

---
+++ src/main/java/com/eleks/academy/whoami/exception/FailGetPlayersNameException.java
@@ -0,0 +1,18 @@
+package com.eleks.academy.whoami.exception;
+
+public class FailGetPlayersNameException extends RuntimeException {
+    public FailGetPlayersNameException() {
+    }
+
+    public FailGetPlayersNameException(String message) {
+        super(message);
+    }
+
+    public FailGetPlayersNameException(Throwable cause) {
+        super(cause);
+    }
+
+    public FailGetPlayersNameException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+}
