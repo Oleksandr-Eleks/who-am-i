f0573ee51d6674ed4ff6baecf1646d771e6d6d9d
Dmitry <dmitrymishnev@gmail.com>
2022-05-10 17:35:33 +0300

    update

--- src/main/java/com/eleks/academy/whoami/App.java
+++ src/main/java/com/eleks/academy/whoami/App.java
@@ -1,23 +1,10 @@
 package com.eleks.academy.whoami;
 
-import com.eleks.academy.whoami.configuration.ContextConfig;
-import com.eleks.academy.whoami.configuration.ServerProperties;
-import com.eleks.academy.whoami.core.Game;
-import com.eleks.academy.whoami.networking.server.Server;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
 import java.io.IOException;
 
 public class App {
 
-    public static void main(String[] args) throws IOException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(ContextConfig.class);
-        ServerProperties properties = context.getBean(ServerProperties.class);
-        Server server = context.getBean(Server.class);
-
-        Game game = server.startGame();
-        game.play();
-    }
+	public static void main(String[] args) throws IOException {
+	}
 
 }
--- src/main/java/com/eleks/academy/whoami/exception/PlayerAnswerException.java
+++
@@ -1,8 +0,0 @@
-package com.eleks.academy.whoami.exception;
-
-public class PlayerAnswerException extends RuntimeException {
-    public PlayerAnswerException(String message, Throwable cause) {
-        super(message, cause);
-        cause.printStackTrace();
-    }
-}
