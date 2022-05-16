5751a810be82ff2f9777e9c57b7ae022f5bb2670
MinoUni <maks.reva.ua@gmail.com>
2022-05-16 23:14:20 +0300

    fix test findAvailableGames

--- src/test/java/com/eleks/academy/whoami/service/impl/GameServiceTest.java
+++ src/test/java/com/eleks/academy/whoami/service/impl/GameServiceTest.java
@@ -6,16 +6,20 @@ import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.List;
-import java.util.function.Supplier;
 import java.util.stream.Stream;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.eleks.academy.whoami.core.SynchronousGame;
+import com.eleks.academy.whoami.core.impl.PersistentGame;
+import com.eleks.academy.whoami.model.request.NewGameRequest;
+import com.eleks.academy.whoami.model.response.GameDetails;
 import com.eleks.academy.whoami.model.response.GameLight;
 import com.eleks.academy.whoami.repository.impl.GameInMemoryRepository;
 
@@ -24,22 +28,39 @@ class GameServiceTest {
 
 	private final GameInMemoryRepository mockGameRepository = mock(GameInMemoryRepository.class);	
 	private final GameServiceImpl gameService = new GameServiceImpl(mockGameRepository);
-
+	private final NewGameRequest gameRequest = new NewGameRequest();
+	
+	@BeforeEach
+	void set() {
+		gameRequest.setMaxPlayers(3);
+	}
+	
 	@Test
 	void findAvailableGames() throws Exception {
 		final String player = "player";
-		final Supplier<Stream<SynchronousGame>> games = () -> Stream.empty();
-		doReturn(games.get()).when(mockGameRepository).findAllAvailable(player);
+		Stream<SynchronousGame> games = Stream.empty();
 		
-		final List<GameLight> expected = gameService.findAvailableGames(player);
-		assertThat(expected).isEqualTo(games.get().map(GameLight::of).toList());
-		assertThat(expected).isEmpty();
+		when(mockGameRepository.findAllAvailable(player)).thenReturn(games);
+		
+		List<GameLight> listOfGames = gameService.findAvailableGames(player);
+		
+		assertThat(listOfGames).isNotNull();
+		assertThat(listOfGames).isEmpty();
 		
 		verify(mockGameRepository, times(1)).findAllAvailable(eq(player));
 	}
 
 	@Test
 	void createGame() {
+		final String player = "player";
+		final var game = new PersistentGame(player, gameRequest.getMaxPlayers());
 		
+		when(mockGameRepository.save(game)).thenReturn(new PersistentGame(player, gameRequest.getMaxPlayers()));
+		
+		GameDetails createdGame = gameService.createGame(player, gameRequest);
+		
+		assertThat(createdGame).isNotNull();
+		
+		verify(mockGameRepository, times(1)).save(eq(game));
 	}
 }
