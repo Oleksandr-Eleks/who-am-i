b316faf9a4b3921ddca67b6d26ea488454566d5a
Dmitry <dmitrymishnev@gmail.com>
2022-05-16 18:55:39 +0300

    resolve conflict merge

---
+++ src/test/java/com/eleks/academy/whoami/service/impl/GameServiceImplTest.java
@@ -0,0 +1,12 @@
+package com.eleks.academy.whoami.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameServiceImplTest {
+
+    @Test
+    void findAvailableGames() {
+    }
+}
