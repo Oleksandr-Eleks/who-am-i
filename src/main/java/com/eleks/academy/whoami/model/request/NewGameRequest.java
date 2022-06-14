53fca437d3af51870c5e74e8bdb9a5a0a1cc20f9
Yurii <yurii.lypchanskyi@gmail.com>
2022-06-14 15:06:00 +0300

    Change the value of variable maxPlayers

--- src/main/java/com/eleks/academy/whoami/model/request/NewGameRequest.java
+++ src/main/java/com/eleks/academy/whoami/model/request/NewGameRequest.java
@@ -14,6 +14,7 @@ import javax.validation.constraints.NotNull;
 public class NewGameRequest {
 
 	//TODO: Retrieve from config
+
 	@Min(4)
 	@Max(6)
 	@NotNull(message = "maxPlayers must not be null")
