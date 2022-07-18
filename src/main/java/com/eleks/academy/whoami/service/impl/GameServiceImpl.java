5df223656b5eecb6398894a62c244533185f6347
Alina Yadvinska <tonks1997@gmail.com>
2022-07-18 15:04:19 +0300

    renamed showPlayers

--- src/main/java/com/eleks/academy/whoami/service/impl/GameServiceImpl.java
+++ src/main/java/com/eleks/academy/whoami/service/impl/GameServiceImpl.java
@@ -173,7 +173,7 @@ public class GameServiceImpl implements GameService {
     }
 
     @Override
-    public int getAllPlayersCount() {
+    public int getAllPlayers() {
         List<PersistentGame> allGames = findAllGames();
         int allPlayers = 0;
         if (!allGames.isEmpty()) {
